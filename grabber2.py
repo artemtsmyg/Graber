9002a887b362f2e0e75a12917434a5b2f4ad771b
Артём Цмыг <129445769+artemtsmyg@users.noreply.github.com>
2024-11-27 00:14:34 +0300

    Update grabber2.py

--- grabber2.py
+++ grabber2.py
@@ -1,1 +1,10 @@
+import requests
+url = "https://jsonplaceholder.typicode.com/todos"  # Пример открытого API
 
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    for item in data:
+        print(f"ID: {item['id']}, Title: {item['title']}")
+else:
+    print(f"Ошибка: {response.status_code}")
